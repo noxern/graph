71c139a2ba830d925f29456309d510548cedb759
Mikkel Nielsen <noxern@gmail.com>
2017-04-24 20:16:23 +0200

    Now using hug.rest and Python 3.6

--- README.md
+++ README.md
@@ -1,23 +1,66 @@
 # graph
-A Python tool for generating IMDb rating graphs of TV series using [IMDb](http://www.imdb.com) and [Plotly](https://plot.ly).
+A Python 3.6 API for generating IMDb rating graphs of TV series using [IMDb](http://www.imdb.com) and [Plotly](https://plot.ly).
 
-### Usage
+
+### Config
+
+Environmental variables:
+- `PLOTLY_USERNAME`
+- `PLOTLY_PASSWORD`
+
+### Setup
+
+0. (Optional) Run `virtualenv -p python3.6 venv` to set up a Python 3.6 virtual environment.
+0. (Optional) Run `source venv/bin/activate` to activate it.
+0. Run `pip install -r requirements.txt` to install the dependencies.
+
+### Running
+
+0. Run `hug -f graph.py` to start a server.
+0. Navigate to http://localhost:8000 to access the API.
+
+### Endpoints
+
+The documentation for the API are returned as JSON on every 404 request. For example on localhost:
+
+```json
+{
+    "404": "The API call you tried to make was not defined. Here's a definition of the API to help you get going :)",
+    "documentation": {
+        "handlers": {
+            "/graph": {
+                "GET": {
+                    "usage": "Returns an IMDb ratings graph of the given TV series",
+                    "examples": [
+                        "http://localhost:8000/graph?title=Breaking%20Bad"
+                    ],
+                    "outputs": {
+                        "format": null,
+                        "content_type": "image/png"
+                    },
+                    "inputs": {
+                        "title": {
+                            "type": "Basic text / string value"
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
 ```
-usage: graph.py [-h] title username password
 
-positional arguments:
-  title       The title of the TV series
-  username    The username of your Plotly account
-  password    The password of your Plotly account
+### Example
 
-optional arguments:
-  -h, --help  show this help message and exit
+#### Request
+```text
+GET /graph?title=Breaking%20Bad
 ```
 
-### Example input
-```
-$ python graph.py 'Breaking Bad' 'johndoe' 'Tr0ub4dor&3'
+#### Response
+```text
+HTTP/1.1 200 OK
+Content-Type: image/png
 ```
 
-### Example output
 ![Graph](graph.png)
--- graph.py
+++ graph.py
@@ -1,78 +1,95 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3.6
 
-import argparse
-import sys
+import os
 
+import falcon
+import hug
+import hug.types
 import plotly.graph_objs as go
 import plotly.plotly as py
-import six
-from lxml.html import parse
+import requests
+from lxml import html
 
 IMDB_URL = 'http://www.imdb.com'
 
-parser = argparse.ArgumentParser()
-parser.add_argument('title', help='The title of the TV series')
-parser.add_argument('username', help='The username of your Plotly account')
-parser.add_argument('password', help='The password of your Plotly account')
-args = parser.parse_args()
+py.sign_in(os.environ['PLOTLY_USERNAME'], os.environ['PLOTLY_PASSWORD'])
 
-# find a candidate
-search_page = parse(IMDB_URL + '/find?q=%s&s=tt&ttype=tv' % args.title)
-candidate = search_page.xpath('//*[@class="findSection"]/table/tr[1]/td[2]/a')
-if not candidate: sys.exit('Oh no! No TV series was found with the name: %s' % args.title)
+api = hug.API(__name__)
 
-# get ratings
-ratings_page = parse(IMDB_URL + '/title/%s/epdate' % candidate[0].get('href').split('/')[2])
-rows = ratings_page.xpath('//*[@id="tn15content"]/table//tr[descendant::td]')
-if not rows: sys.exit('Oh no! No ratings were found for: %s' % candidate[0].text)
 
-# parse ratings
-results = {}
+@hug.output_format.on_valid('image/png')
+def format_as_png_when_valid(data, request=None, response=None):
+    return data
 
-for row in rows:
 
-    if '.' not in row[0].text: continue  # episode doesn't belong in a season (eg. special)
-    if len(row) != 5: continue           # episode hasn't aired yet
+@hug.get(output=format_as_png_when_valid, examples='title=Breaking%20Bad')
+def graph(title: hug.types.text):
+    """Returns an IMDb ratings graph of the given TV series"""
 
-    season, episode = map(int, row[0].text.split('.'))
-    title = row[1][0].text
-    link = IMDB_URL + row[1][0].get('href')
-    rating = float(row[2].text)
-    votes = int(row[3].text.replace(',', ''))
+    # find a candidate (with English as accept language to avoid geolocalized title names)
+    search_res = requests.get(IMDB_URL + f'/find?q={title}&s=tt&ttype=tv', headers={'Accept-Language': 'en'})
+    search_page = html.fromstring(search_res.text)
+    candidate = search_page.xpath('//*[@class="findSection"]/table/tr[1]/td[2]/a')
+    if not candidate: raise Exception(f'Oh no! No TV series was found with the name: {title}')
 
-    results.setdefault(season, []).append(rating)
+    tt_id = candidate[0].get("href").split("/")[2]
+    title = candidate[0].text
 
-# create graph data
-data = []
-episodes = 0
+    # get ratings
+    ratings_res = requests.get(IMDB_URL + f'/title/{tt_id}/epdate')
+    ratings_page = html.fromstring(ratings_res.text)
+    rows = ratings_page.xpath('//*[@id="tn15content"]/table//tr[descendant::td]')
+    if not rows: raise Exception(f'Oh no! No ratings were found for: {title}')
 
-for season, ratings in six.iteritems(results):
-    data.append(go.Scatter(
-        name='S' + str(season),
-        x=list(range(episodes+1, episodes+len(ratings)+1)),
-        y=ratings,
-        marker=dict(size=5)
-    ))
+    # parse ratings
+    results = {}
 
-    episodes += len(ratings)
+    for row in rows:
 
-# set up layout
-layout = go.Layout(
-    title='<b>IMDb ratings of %s episodes</b>' % candidate[0].text,
-    yaxis=dict(title='Rating', range=[0, 10.1], tickmode='linear', tick0=0,
-               dtick=2.5, tickformat='.1f', tickprefix=' ' * 10),
-    xaxis=dict(title='Episode', range=[0, episodes+1], tickmode='array',
-               tickvals=[1, episodes], showgrid=False),
-    margin=go.Margin(l=100, pad=10),
-    showlegend=False,
-    width=1200,
-    height=400
-)
+        if '.' not in row[0].text: continue  # episode doesn't belong in a season (eg. special)
+        if len(row) != 5: continue           # episode hasn't aired yet
 
-# sign in to plot.ly and output png
-try:
-    py.sign_in(args.username, args.password)
+        season, episode = map(int, row[0].text.split('.'))
+        ep_title = row[1][0].text
+        ep_link = IMDB_URL + row[1][0].get('href')
+        ep_rating = float(row[2].text)
+        ep_votes = int(row[3].text.replace(',', ''))
+
+        results.setdefault(season, []).append(ep_rating)
+
+    # create graph data
+    data = []
+    episodes = 0
+
+    for season, ratings in results.items():
+        data.append(go.Scatter(
+            name='S' + str(season),
+            x=list(range(episodes + 1, episodes + len(ratings) + 1)),
+            y=ratings,
+            marker=dict(size=5)
+        ))
+
+        episodes += len(ratings)
+
+    # set up layout
+    layout = go.Layout(
+        title=f'<b>IMDb ratings of {title} episodes</b>',
+        yaxis=dict(title='Rating', range=[0, 10.1], tickmode='linear', tick0=0,
+                   dtick=2.5, tickformat='.1f', tickprefix=' ' * 10),
+        xaxis=dict(title='Episode', range=[0, episodes + 1], tickmode='array',
+                   tickvals=[1, episodes], showgrid=False),
+        margin=go.Margin(l=100, pad=10),
+        showlegend=False,
+        width=1200,
+        height=400
+    )
+
     fig = go.Figure(data=data, layout=layout)
-    py.image.save_as(fig, filename='graph.png')
-except Exception:
-    sys.exit('Oh no! Something has gone wrong.')
+    output = py.image.get(fig)
+
+    return output
+
+
+@hug.exception(Exception)
+def handle_exception(exception):
+    raise falcon.HTTPInternalServerError('error', str(exception))
